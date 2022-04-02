aa616fbec1e2e9818af25fcff03cb608a8429216
rahulsampa <91297988+rahulsampa@users.noreply.github.com>
2022-04-02 17:38:17 -0600

    Update project2.m

--- project2.m
+++ project2.m
@@ -19,6 +19,7 @@ mariana_longitude = importdata('mariana_longitude.csv')';
 depth = griddata(mariana_longitude, mariana_latitude, mariana_depth,longitude,latitude);
 
 %% Contour Plot of Mariana Trench Depths 
+figure(1)
 contourf(longitude, latitude, depth) 
 title('Mariana Trench Depths at Different Locations')
 xlim([138.9 150])
@@ -29,6 +30,58 @@ colorbar
 %% Function Calls
 [Deepest_Point_Sampled, Deepest_Latitude, Deepest_Longitude] = calc_maxdepth(mariana_depth, mariana_latitude, mariana_longitude);
 [Mean_Depth_In_Km] = calc_meandepth(mariana_depth) 
+
+%% Eigenvalue Calculations 
+unit_vec = randi(10,1440,1);
+unit_vec = unit_vec/norm(unit_vec);
+
+
+AT_A = mariana_depth * mariana_depth'; 
+
+
+for i = 2:10
+
+unit_vec(:,i) = AT_A * unit_vec(:,i-1) / norm(AT_A * unit_vec(:,i-1));
+
+    if norm(unit_vec(:,i) - unit_vec(:,i-1)) < 0.00001
+    
+        break; 
+    
+    end
+ 
+ V1 = unit_vec(:,end); 
+
+end 
+
+N = [1:size(V1)]; 
+figure(2)
+plot(N,V1)
+title('Eigenvector vs. N Location')
+xlabel('Location') 
+ylabel('Eigenvector Value') 
+
+%% Eigenvector Calculations 
+% V50 = zeros(1,50); 
+% 
+% for i = 2:50 
+%     u_1 = randi(10,1440,1);
+%     u_1 = u_1 / norm(u_1); 
+%     
+%     u_1(:,i) = AT_A * u_1(:,i-1); 
+%     
+%     for j = 1:i-1 
+%     u_1(:,i) =  u_1(:,i-1) - symsum(u_1(:,i)' * V50(j)*V50(j),j,1,i-1);
+%     end 
+%     
+%     u_1(:,i) = u_1(:,i)/norm(u_1(:,i))
+%     
+%     if norm(u_1(:,i) - u_1(:,i-1)) < 10^-3
+%         
+%         u_1(:,i) = V50(:,i) 
+%         break; 
+%     end 
+%     
+% end 
 
 %% Deepest Point Function 
 function [Deepest_Point_Sampled, Deepest_Latitude, Deepest_Longitude] = calc_maxdepth(mariana_depth, mariana_latitude, mariana_longitude)
@@ -69,6 +122,8 @@ r = 1;
      Mean_Depth_In_Km = mean(nominal_depth_vec)/1000; % In km
 
 end 
+
+
 
 
 
